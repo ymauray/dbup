81d7305bf7855b8b93a4b16e538940c98165c2d0
Yannick Mauray <yannick.mauray@gmail.com>
2024-05-30 13:15:35 +0200

    feat: upgraded phpunit and all tests

--- tests/Dbup/Tests/ApplicationTest.php
+++ tests/Dbup/Tests/ApplicationTest.php
@@ -2,22 +2,33 @@
 namespace Dbup\Tests;
 
 use Dbup\Application;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 
-Class ApplicationTest extends \PHPUnit_Framework_TestCase
+Class ApplicationTest extends TestCase
 {
     public $app;
     public $pdo;
 
-    public function setUP()
+    private \PDO $dbh;
+    private \PDOStatement $stmt;
+
+    public function setUp() : void
     {
         $this->app = \Phake::partialMock('Dbup\Application');
         $this->pdo = \Phake::mock('Dbup\Database\PdoDatabase');
-        $this->dbh = \Phake::mock('\Pdo');
+        $this->dbh = \Phake::mock('\PDO');
         \Phake::when($this->pdo)->connection(\Phake::anyParameters())->thenReturn($this->dbh);
         $this->stmt = \Phake::mock('\PDOStatement');
         \Phake::when($this->dbh)->prepare(\Phake::anyParameters())->thenReturn($this->stmt);
+
+        \Hamcrest\Util::registerGlobalFunctions();
+    }
+
+    public function tearDown(): void
+    {
+        $this->addToAssertionCount(\Hamcrest\MatcherAssert::getCount());
     }
 
     public function testSetPropertiesWhenInstanceIsMade()
@@ -38,7 +49,7 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
         $_SERVER['DBUP_TEST_HOST'] = 'replaced_host';
         $_SERVER['OTHER_PREFIX_DBUP_TEST_PASSWORD'] = 'replaced_password';
 
-        $ini = __DIR__ . '/.dbup/properties.ini.replace';
+        $ini = __DIR__ . '/../.dbup/properties.ini.replace';
         $parsed = $this->app->parseIniFile($ini)['pdo'];
 
         assertThat($parsed['dsn'], is('replaced_dbms:dbname=replaced_user_replaced_host_%%DBUP_TEST_NOT_REPLACED%%;host=replaced_host'));
@@ -48,7 +59,7 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testSetConfigFromIni()
     {
-        $ini = __DIR__ . '/.dbup/properties.ini';
+        $ini = __DIR__ . '/../.dbup/properties.ini';
         $this->app->setConfigFromIni($ini);
 
         assertThat($this->app->sqlFilesDir, is('/etc/dbup/sql'));
@@ -56,18 +67,16 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
         \Phake::verify($this->app)->createPdo('mysql:dbname=testdatabase;host=localhost', 'testuser', 'testpassword', ['\PDO::MYSQL_ATTR_LOCAL_INFILE' => 1]);
     }
 
-    /**
-     * @expectedException Dbup\Exception\RuntimeException
-     */
     public function testCatchExceptionSetConfigFromEmptyIni()
     {
-        $ini = __DIR__ . '/.dbup/properties.ini.empty';
+        $this->expectException(\Dbup\Exception\RuntimeException::class);
+        $ini = __DIR__ . '/../.dbup/properties.ini.empty';
         $this->app->setConfigFromIni($ini);
     }
 
     public function testSetConfigFromMinIni()
     {
-        $ini = __DIR__ . '/.dbup/properties.ini.min';
+        $ini = __DIR__ . '/../.dbup/properties.ini.min';
         $this->app->setConfigFromIni($ini);
 
         \Phake::verify($this->app)->createPdo('mysql:dbname=testdatabase;host=localhost', '', '', []);
@@ -75,24 +84,22 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testGetSqlFileByName()
     {
-        $this->app->sqlFilesDir = __DIR__ . '/sql';
+        $this->app->sqlFilesDir = __DIR__ . '/../sql';
         $file = $this->app->getSqlFileByName('V1__sample_select.sql');
         assertThat($file->getFileName(), is('V1__sample_select.sql'));
     }
 
-    /**
-     * @expectedException Dbup\Exception\RuntimeException
-     */
     public function testCatchExceptionWhenNotFoundSqlFileByName()
     {
-        $this->app->sqlFilesDir = __DIR__ . '/sql';
+        $this->expectException(\Dbup\Exception\RuntimeException::class);
+        $this->app->sqlFilesDir = __DIR__ . '/../sql';
         $this->app->getSqlFileByName('hoge');
     }
 
     public function testGetStatuses()
     {
-        $this->app->sqlFilesDir = __DIR__ . '/sql';
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
+        $this->app->sqlFilesDir = __DIR__ . '/../sql';
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
 
         $statuses = $this->app->getStatuses();
 
@@ -107,8 +114,8 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testGetUpCandidates()
     {
-        $this->app->sqlFilesDir = __DIR__ . '/sql';
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
+        $this->app->sqlFilesDir = __DIR__ . '/../sql';
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
 
         $candidates = $this->app->getUpCandidates();
 
@@ -117,63 +124,59 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
         assertThat($candidates[1]->file->getFileName(), is('V12__sample12_select.sql'));
     }
 
-    /**
-     * @expectedException Dbup\Exception\RuntimeException
-     */
     public function testCatchExceptionCopyToAppliedDir()
     {
-        $this->app->appliedFilesDir = __DIR__ . '/nondir';
-        $file = new \SplFileInfo(__DIR__ . '/samples/plural.sql');
+        $this->expectException(\Dbup\Exception\RuntimeException::class);
+        $this->app->appliedFilesDir = __DIR__ . '/../../nondir';
+        $file = new \SplFileInfo(__DIR__ . '/../samples/plural.sql');
         $this->app->copyToAppliedDir($file);
     }
 
     public function testCopyToAppliedDir()
     {
-        @unlink(__DIR__ . '/.dbup/applied/single.sql');
+        @unlink(__DIR__ . '/../.dbup/applied/single.sql');
 
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
-        $file = new \SplFileInfo(__DIR__ . '/samples/single.sql');
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
+        $file = new \SplFileInfo(__DIR__ . '/../samples/single.sql');
         $this->app->copyToAppliedDir($file);
 
-        assertThat(file_exists(__DIR__ . '/.dbup/applied/single.sql'), is(true));
+        assertThat(file_exists(__DIR__ . '/../.dbup/applied/single.sql'), is(true));
 
-        @unlink(__DIR__ . '/.dbup/applied/single.sql');
+        @unlink(__DIR__ . '/../.dbup/applied/single.sql');
     }
 
     public function testUpWithSingleStatementSqlFile()
     {
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
                 $this->app->pdo = $this->pdo;
-        $file = new \SplFileInfo(__DIR__ . '/samples/single.sql');
+        $file = new \SplFileInfo(__DIR__ . '/../samples/single.sql');
 
         $this->app->up($file);
 
         \Phake::verify($this->dbh, \Phake::times(1))->prepare('select 1+1');
 
-        @unlink(__DIR__ . '/.dbup/applied/single.sql');
+        @unlink(__DIR__ . '/../.dbup/applied/single.sql');
     }
 
     public function testUpWithPluralStatementsSqlFile()
     {
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
         $this->app->pdo = $this->pdo;
-        $file = new \SplFileInfo(__DIR__ . '/samples/plural.sql');
+        $file = new \SplFileInfo(__DIR__ . '/../samples/plural.sql');
 
         $this->app->up($file);
 
         \Phake::verify($this->dbh, \Phake::times(1))->prepare('select 1+1');
         \Phake::verify($this->dbh, \Phake::times(1))->prepare('select 2+2');
 
-        @unlink(__DIR__ . '/.dbup/applied/plural.sql');
+        @unlink(__DIR__ . '/../.dbup/applied/plural.sql');
     }
 
-    /**
-     * @expectedException Dbup\Exception\RuntimeException
-     */
     public function testCatchExceptionWhenUp()
     {
+        $this->expectException(\Dbup\Exception\RuntimeException::class);
         $this->app->pdo = $this->pdo;
-        $file = new \SplFileInfo(__DIR__ . '/samples/single.sql');
+        $file = new \SplFileInfo(__DIR__ . '/../samples/single.sql');
 
         \Phake::when($this->dbh)->prepare(\Phake::anyParameters())->thenThrow(new \PDOException);
 
@@ -185,14 +188,14 @@ Class ApplicationTest extends \PHPUnit_Framework_TestCase
      */
     public function testUpWithSingleStatementWithEmptyLineSqlFile()
     {
-        $this->app->appliedFilesDir = __DIR__ . '/.dbup/applied';
+        $this->app->appliedFilesDir = __DIR__ . '/../.dbup/applied';
         $this->app->pdo = $this->pdo;
-        $file = new \SplFileInfo(__DIR__ . '/samples/singleWithEmpty.sql');
+        $file = new \SplFileInfo(__DIR__ . '/../samples/singleWithEmpty.sql');
 
         $this->app->up($file);
 
         \Phake::verify($this->dbh, \Phake::times(1))->prepare(\Phake::anyParameters());
 
-        @unlink(__DIR__ . '/.dbup/applied/single.sql');
+        @unlink(__DIR__ . '/../.dbup/applied/single.sql');
     }
 }
--- tests/Dbup/Tests/Command/StatusCommandTest.php
+++ tests/Dbup/Tests/Command/StatusCommandTest.php
@@ -2,11 +2,22 @@
 namespace Dbup\Tests\Command;
 
 use Dbup\Application;
+use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Tester\CommandTester;
 use Dbup\Command\StatusCommand;
 
-class StatusCommandTest extends \PHPUnit_Framework_TestCase
+class StatusCommandTest extends TestCase
 {
+    public function setUp() : void
+    {
+        \Hamcrest\Util::registerGlobalFunctions();
+    }
+
+    public function tearDown(): void
+    {
+        $this->addToAssertionCount(\Hamcrest\MatcherAssert::getCount());
+    }
+
     public function testSpecificPropertiesIni()
     {
         $application = new Application();
@@ -15,17 +26,15 @@ class StatusCommandTest extends \PHPUnit_Framework_TestCase
         $command = $application->find('status');
         $commandTester = new CommandTester($command);
         $commandTester->execute(['command' => $command->getName(),
-                '--ini' => __DIR__ . '/../.dbup/properties.ini.test',
+                '--ini' => __DIR__ . '/../../.dbup/properties.ini.test',
             ]);
 
         assertThat($commandTester->getDisplay(), is(containsString('| appending...        | V12__sample12_select.sql |')));
     }
 
-    /**
-     * @expectedException Dbup\Exception\RuntimeException
-     */
     public function testCatchExceptionNonExistIni()
     {
+        $this->expectException(\Dbup\Exception\RuntimeException::class);
         $application = new Application();
         $application->add(new StatusCommand());
 
